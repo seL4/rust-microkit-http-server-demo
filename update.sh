94476f6b7a986a7709e4e67cdbe34c31807f0242
Nick Spinale <nick@nickspinale.com>
2023-05-24 05:17:46 +0100

    Create update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+set -eu
+
+external_rust_seL4_dir=../../rust-seL4
+external_banscii_dir=$external_rust_seL4_dir/crates/examples/sel4cp/banscii
+
+cp $external_rust_seL4_dir/rust-toolchain.toml .
+cp $external_rust_seL4_dir/support/targets/aarch64-sel4cp-minimal.json support/targets
+cp $external_banscii_dir/banscii.system .
+rm -r crates && cp -r $external_banscii_dir/pds crates
